98571416db4a600696530bba57e562c282935f50
victorSlavila <DrumuridemuntE12>
2021-04-20 18:49:14 +0300

    add cors origin and display the list of products

--- backend-eCommerce/src/main/java/eu/victor/ro/springbootecommerce/repo/ProductRepository.java
+++ backend-eCommerce/src/main/java/eu/victor/ro/springbootecommerce/repo/ProductRepository.java
@@ -2,6 +2,8 @@ package eu.victor.ro.springbootecommerce.repo;
 
 import eu.victor.ro.springbootecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
