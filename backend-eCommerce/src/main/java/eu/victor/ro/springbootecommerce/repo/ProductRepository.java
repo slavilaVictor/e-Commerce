cc3fb57a5322bfc11c51f7e699562aef3999edaf
victorSlavila <DrumuridemuntE12>
2021-04-28 16:43:50 +0300

    add routing and query method

--- backend-eCommerce/src/main/java/eu/victor/ro/springbootecommerce/repo/ProductRepository.java
+++ backend-eCommerce/src/main/java/eu/victor/ro/springbootecommerce/repo/ProductRepository.java
@@ -1,9 +1,17 @@
 package eu.victor.ro.springbootecommerce.repo;
 
 import eu.victor.ro.springbootecommerce.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    // add a query method
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    // !! Spring Data REST automatically exposes the endpoint : "http://localhost:8080/api/products/search/findByCategoryId" !!
+    // To verify : http://localhost:8080/api/products/search/findByCategoryId?id=1
 }
--- frontend-eCommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend-eCommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,15 +11,34 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[]
+  currentCategoryId: number
 
-  constructor(private productService: ProductService) { }
+  // ActivatedRoute -> the current active route that loaded the component
+  // It helps me accessing route parameters
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts()
+    this.route.paramMap.subscribe( () => {
+      this.listProducts()
+    })
   }
 
   listProducts(): void{
-    this.productService.getProductList().subscribe(
+    // check if "id" parameter is available
+    // I use the activated route(route), snapshot gives me the current state of this route at a given moment in time
+    // paramMap is the map of all paramters and I search for the "id" parameter
+    // if the parameter "id" is available, it will return true; else returns false
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id')
+    if(hasCategoryId){
+      // get the "id" parameter string and the convert it to a numbr using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')
+    }else{
+      // default category with id=1
+      this.currentCategoryId = 1
+    }
+    // get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data
       }
